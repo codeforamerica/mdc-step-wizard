37c99e0f29ecd05807a80ac37e066a80b57eaaf9
phiden <sdengo@codeforamerica.org>
2015-07-13 15:06:20 -0700

    added spinning loader to geocoder. Closes #33

--- app.js
+++ app.js
@@ -8,6 +8,31 @@ $(document).ready(function() {
 	var currentNode;
 	var sections = [];
 	
+	var opts = {
+		  lines: 11 // The number of lines to draw
+		, length: 0 // The length of each line
+		, width: 20 // The line thickness
+		, radius: 49 // The radius of the inner circle
+		, scale: 0.5 // Scales overall size of the spinner
+		, corners: 1 // Corner roundness (0..1)
+		, color: '#000' // #rgb or #rrggbb or array of colors
+		, opacity: 0.25 // Opacity of the lines
+		, rotate: 90 // The rotation offset
+		, direction: 1 // 1: clockwise, -1: counterclockwise
+		, speed: 0.9 // Rounds per second
+		, trail: 60 // Afterglow percentage
+		, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+		, zIndex: 2e9 // The z-index (defaults to 2000000000)
+		, className: 'spinner' // The CSS class to assign to the spinner
+		, top: '50%' // Top position relative to parent
+		, left: '50%' // Left position relative to parent
+		, shadow: false // Whether to render a shadow
+		, hwaccel: false // Whether to use hardware acceleration
+		, position: 'absolute' // Element positioning
+	}
+	
+	
+	
 	function labelSections() {
 		
 		$('section').each(function() {
@@ -314,7 +339,7 @@ $(document).ready(function() {
 			case 'reset':
 			
 				location.reload();
-				break;
+				break;	
 			
 		}
 	}
@@ -329,7 +354,9 @@ $(document).ready(function() {
 	   //hacky loader to give user feedback
 	   $("#loader").removeClass('hidden');
 	   $('#loader').html("<h3>Please wait</h3><p>We're checking to see if your address is in unincorporated Miami-Dade County.</p>");
-	   
+	   var target = document.getElementById('loader')
+	   var spinner = new Spinner(opts).spin(target);
+	   console.log(target, spinner);
 	    geocoder.geocode( { 'address': address}, function(results, status) {
 		    
 	    	if (status == google.maps.GeocoderStatus.OK) {
@@ -387,7 +414,7 @@ $(document).ready(function() {
 		console.log('show UMSA:', municipality);
 		
 		//hide hacky loader
-		$('#loader').addClass('hidden');
+		//$('#loader').addClass('hidden');
 		
 		var txt = $('#address-value .value').text();
 		console.log('the address is:' , txt);
